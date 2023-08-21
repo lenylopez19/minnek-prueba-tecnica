77cf0e3c05ded23eca1ce09975ba388137614bea
lenylopez19 <leny.lopez19@gmail.com>
2023-08-21 19:09:29 -0400

    error handling, load handling, search function

--- #2/js/engine.js
+++ #2/js/engine.js
@@ -1,12 +1,12 @@
 const API = "https://dog.ceo/api";
 const listAllDogs = "breeds/list/all";
+let globalDogs = [];
 
 function getRamdonDog(dogArr, maxDogs) {
   let newDogArr = [];
   let numberOfDogs = dogArr.length - 1;
   for (let i = 0; i < maxDogs; i++) {
     const randomDog = Math.round(Math.random() * numberOfDogs);
-
     if (!newDogArr.includes(dogArr[randomDog]))
       newDogArr.push(dogArr[randomDog]);
     else i--;
@@ -14,47 +14,73 @@ function getRamdonDog(dogArr, maxDogs) {
   return newDogArr;
 }
 
-const getDogs = async (numberOfDogs) => {
+const getDogs = async () => {
+  showLoading();
   let result;
-
   try {
     result = await fetch(`${API}/${listAllDogs}`);
-  } catch {
-    console.error("something went wrong fetching the dogs info", err);
+  } catch (err) {
+    hideLoading();
+    showError("Woops, something went wrong.");
+    console.error("Error fetching the dogs info", err);
   }
 
   if (result?.ok) {
     const dataJson = await result.json();
-    const dogs = Object.entries(dataJson.message);
-    const dogsToDisplay = getRamdonDog(dogs, numberOfDogs);
-
-    for (const dog of dogsToDisplay) {
-      const dogName = dog[0];
-      try {
-        result = await fetch(`${API}/breed/${dogName}/images/random`);
-      } catch (err) {
-        console.error("something went wrong fetching a dog image", err);
-      }
-      if (result?.ok) {
-        const dataJson = await result.json();
-        const dogImageUrl = dataJson.message;
-        renderDogs(dog, dogImageUrl, 3);
-      } else {
-        console.log(result?.status);
-      }
-    }
-  } else {
-    console.log(`HTTP Response Code: ${result?.status}`);
+    globalDogs = Object.entries(dataJson.message);
+    prepareDog();
+  } 
+  else {
+    hideLoading();
+    showError("Woops, something went wrong.");
+    console.error(
+      `Error fetching the dogs info, HTTP ResponseCode: ${result?.status}`
+    );
   }
 };
 
-function renderDogs(dog, dogImageUrl, breedLimit) {
-  const breeds = dog[1];
-  const dogName = dog[0];
+function prepareDog(){
+  showLoading();
+  const dogsToDisplay = getRamdonDog(globalDogs, homeDogs);
+    for (const dog of dogsToDisplay) {
+      const [dogName, dogBreeds] = dog;
+      const dogObj = {
+        name: dogName,
+        breed: dogBreeds,
+      };
+      getDogsImg(dogObj, renderDogs);
+    }
+}
+
+const getDogsImg = async (dogObj, fn) => {
+  let result;
+  try {
+    result = await fetch(`${API}/breed/${dogObj.name}/images/random`);
+  } catch (err) {
+    hideLoading();
+    showError("Woops, something went wrong.");
+    console.error("error fetching img", err);
+  }
+
+  if (result?.ok) {
+    const dataJson = await result.json();
+    const dogImageUrl = dataJson.message;
+    dogObj.imageUrl = dogImageUrl;
+    fn(dogObj);
+  } 
+  else {
+    hideLoading();
+    showError("Woops, something went wrong.");
+    console.error(
+      `Error fetching the dogs info, HTTP ResponseCode: ${result?.status}`
+    );
+  }
+};
+
+function renderDogs(dogObj, breedLimit=3) {
+  const { name: dogName, breed: breeds, imageUrl: dogImageUrl } = dogObj;
   let breedList = "";
-
   const breedExist = breeds.length;
-
   if (breedExist) {
     for (let i = 0; i < breeds.length; i++) {
       if (i >= breedLimit) break;
@@ -63,24 +89,107 @@ function renderDogs(dog, dogImageUrl, breedLimit) {
   } else {
     breedList = `<li>no breed</li>`;
   }
-
   const htmlCard = `
     <article id = "dog_${dogName}"class="card">
-        <div class="sub-breed">
-            <ul>
-                  ${breedList}
-            </ul>
-        </div>
-        <img onerror="this.src='assets/errorImg.jpeg'" id="IMG_${dogName}" src="${dogImageUrl}" class="dogImage" alt="${dogName}">
-        <h2 class="mainTitle">${dogName}</h2>
+      <div class="sub-breed">
+        <ul>
+          ${breedList}
+        </ul>
+      </div>
+      <img onerror="this.src='assets/errorImg.jpeg'" id="IMG_${dogName}" src="${dogImageUrl}" class="dogImage" alt="${dogName}">
+      <h2 class="mainTitle">${dogName}</h2>
     </article>
     `;
-
-  document.getElementById("contentHolder").innerHTML += htmlCard;
-
+  hideLoading();
+  mainContent.innerHTML += htmlCard;
   setTimeout(() => {
-    document.getElementById(`dog_${dogName}`).style.opacity = 1;
-  }, 1000);
+    dogitem = document.getElementById(`dog_${dogName}`)
+    if (dogitem) dogitem.style.opacity = 1;
+  }, 200);
 }
 
-getDogs(8);
+function search(char) {
+  showLoading();
+  char = char.toLowerCase();
+  const result = [...globalDogs].filter((dog) => dog[0].startsWith(char));
+  if (result.length) {
+    for (const dog of result) {
+      const newDog = {
+        name: dog[0],
+        breed: dog[1],
+      };
+      getDogsImg(newDog, renderDogs);
+    }
+  } 
+  else {
+    hideLoading();
+    mainContent.innerHTML = `
+    <div class="searchNotFound"> 
+      <img src='assets/noResult.png' alt = 'no result found...' />
+      <h2>Woops, couldnt find anything.</h2>
+    </div> 
+    `
+  }
+}
+
+const searchInput = document.getElementById("searchDog");
+searchInput.addEventListener("keypress", function (event) {
+  if (event.key == "Enter" && this.value) {
+      mainContent.innerHTML = "";
+      search(this.value);
+  }
+});
+
+function showError(errorMsg) {
+  const errorMsjExist = mainContent.querySelector(".errorMsj");
+  if (!errorMsjExist) {
+    const errorMsjElement = document.createElement("div");
+    errorMsjElement.className = "errorMsj";
+    const html =
+    `
+    <img src="assets/loadingError.png" alt="error" />
+    <h2 class="errorTittle">${errorMsg}</h2>
+    `;
+    errorMsjElement.innerHTML = html;
+    const tryAgainBtn = document.createElement("button");
+    tryAgainBtn.className = "reload";
+    tryAgainBtn.innerText = "Try Again";
+    tryAgainBtn.addEventListener("click", init, { once: true });
+    errorMsjElement.append(tryAgainBtn);
+    contentHolder.append(errorMsjElement);
+  }
+}
+
+function hideError() {
+  const errorMsjExist = mainContent.querySelector(".errorMsj");
+  if (errorMsjExist) {
+    errorMsjExist.remove();
+  }
+}
+
+function showLoading() {
+  const loading = document.querySelector(".loading");
+  loading.style.display = "flex";
+}
+
+function hideLoading() {
+  const loading = document.querySelector(".loading");
+  loading.style.display = "none";
+}
+
+const homeIcon = document.querySelector(".home");
+homeIcon.addEventListener("click", function () {
+  mainContent.innerHTML = "";
+  prepareDog(homeDogs)
+});
+
+function init() {
+  mainContent.innerHTML = "";
+  getDogs(homeDogs);
+}
+
+const mainContent = document.getElementById("contentHolder");
+const homeDogs = 16;
+
+init();
+
